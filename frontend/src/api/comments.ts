c2acd42a38645319390cb16d73a6f0ce9abb7128
koacow <khoa.cnd@gmail.com>
2024-12-01 22:37:37 -0500

    added error handling to api functions

--- frontend/src/api/comments.ts
+++ frontend/src/api/comments.ts
@@ -5,11 +5,17 @@ const API_URL: string = import.meta.env.VITE_API_URL || 'http://localhost:4000/a
 
 export const getFetchDailyComments: () => Promise<Comment[]> = async () => {
     const response = await axios.get(`${API_URL}/comments`);
+    if (response.status !== 200) {
+        throw new Error('Failed to fetch comments');
+    }
     return response.data;
 };
 
 export const addComment: ( content: string, username: string) => Promise<Comment> = async (content, username) => {
     const body = { content, username };
     const response = await axios.post(`${API_URL}`, body);
+    if (response.status !== 201) {
+        throw new Error('Failed to add comment');
+    }
     return response.data;
 };
--- frontend/src/api/songs.ts
+++ frontend/src/api/songs.ts
@@ -5,5 +5,8 @@ const API_URL: string = import.meta.env.VITE_API_URL || 'http://localhost:4000';
 
 export const fetchDailySongs: () => Promise<Song[]> = async () => {
     const response = await axios.get(`${API_URL}/songs`);
+    if (response.status !== 200) {
+        throw new Error('Failed to fetch songs');
+    }
     return response.data;
 };
