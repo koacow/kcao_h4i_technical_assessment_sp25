f041732c0332bb6dd5079fe71e384e183bd6b189
koacow <khoa.cnd@gmail.com>
2024-12-01 22:36:21 -0500

    renamed songs route to /songs

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -8,7 +8,7 @@ const commentsRouter = require('./routes/comments');
 
 app.use(bodyParser.json());
 app.use(cors());
-app.use('/api/song', songRouter);
+app.use('/api/songs', songRouter);
 app.use('/api/comments', commentsRouter);
 
 app.listen(PORT, () => {
